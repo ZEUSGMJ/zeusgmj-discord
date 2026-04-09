63a1d338714b0bb41e6152487dbd0e72724b1ad1
ZEUSGMJ <underminertheone@gmail.com>
2026-04-09 08:01:45 -0700

    chore(config): add remote image domains

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        pathname: '/external/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.steampowered.com',
+        pathname: '/steamcommunity/public/images/apps/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'steamcdn-a.akamaihd.net',
+        pathname: '/steam/apps/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
